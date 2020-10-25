af55114573dd04ced96bc59fa06230c0796506c2
Perttu Lähteenlahti <perttu.lahteenlahti@qvik.fi>
2020-10-25 20:55:13 +0200

    Initial push

---
+++ base/gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    author: `Hyvinvointivalmennukset`,
+    title: `Hyvinvointivalmennukset`,
+    description: `Hyvinvointivalmennukset`,
+    siteUrl: `https://hyvinvointivalmennukset.nyxo.app`,
+    social: {
+      twitter: `Hyvinvointivalmennukset`,
+      linkedIn: `Hyvinvointivalmennukset`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `@hello-nyxo/gatsby-theme-nyxo-coaching`,
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    "gatsby-plugin-typescript",
+  ],
+};
